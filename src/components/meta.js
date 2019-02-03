9a817119ebbbb6499c3d47ca3683de2a46c523e6
Firman Cahyandi <hello@firmanc.com>
2019-02-03 14:30:46 +1100

    Fix linting

--- src/components/meta.js
+++ src/components/meta.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Head from 'next/head';
+import PropTypes from 'prop-types';
 
 const Meta = ({ title, description }) => (
-  <div>
-    <Head>
-      <title>{ title || 'Next.js Test Title' }</title>
-      <meta name="description" content={description || 'Next.js Test Description'} />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta charSet="utf-8" />
-    </Head>
-  </div>
+  <Head>
+    <title>{title}</title>
+    <meta name="description" content={description} />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+  </Head>
 );
+
+Meta.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+};
+
+Meta.defaultProps = {
+  title: 'Default Title',
+  description: 'Default Description',
+};
 
 export default Meta;
--- src/layouts/default.js
+++ src/layouts/default.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import noop from 'lodash/noop';
 
 import Meta from '../components/meta';
 
 const DefaultLayout = ({ children, meta }) => (
   <div>
-    <Meta props={meta} />
+    <Meta {...meta} />
     {children}
   </div>
 );
+
+DefaultLayout.propTypes = {
+  children: PropTypes.node,
+  meta: PropTypes.shape({
+    title: PropTypes.string,
+    description: PropTypes.string,
+  }),
+};
+
+DefaultLayout.defaultProps = {
+  children: noop,
+  meta: {},
+};
 
 export default DefaultLayout;
